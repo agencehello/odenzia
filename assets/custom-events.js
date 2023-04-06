404a6e8e0d38c89c44bea3a2d5b9d4dd628a408c
audreysurfin <audrey@studioboskant.com>
2023-04-06 16:54:07 +0200

    Custom.js created and linked

--- assets/custom-events.js
+++ assets/custom-events.js
@@ -56,31 +56,3 @@ document.addEventListener("flu:product:quantity-update", function (evt) {
 document.addEventListener("quickview:loaded", function () {
   console.log("Quickview loaded");
 });
-
-/* Additional Studio Boskant : Tabs on product page */
-function openTab(evt, content_id, is_mobile = false) {
-  var i, tabcontent, tablinks;
-  if (!is_mobile) {
-    tabcontent = document.getElementsByClassName("tabcontent-cus");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks-cus");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
-    evt.currentTarget.className += " active";
-    document.getElementById(content_id).style.display = "block";
-  } else {
-    if (evt.currentTarget.classList.contains("active")) {
-      document.getElementById(content_id).style.display = "none";
-      evt.currentTarget.className = evt.currentTarget.className.replace(
-        "active",
-        ""
-      );
-    } else {
-      evt.currentTarget.className += " active";
-      document.getElementById(content_id).style.display = "block";
-    }
-  }
-}
---
+++ assets/custom.js
@@ -0,0 +1,27 @@
+/* Additional Studio Boskant : Tabs on product page */
+function openTab(evt, content_id, is_mobile = false) {
+  var i, tabcontent, tablinks;
+  if (!is_mobile) {
+    tabcontent = document.getElementsByClassName("tabcontent-cus");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks-cus");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    evt.currentTarget.className += " active";
+    document.getElementById(content_id).style.display = "block";
+  } else {
+    if (evt.currentTarget.classList.contains("active")) {
+      document.getElementById(content_id).style.display = "none";
+      evt.currentTarget.className = evt.currentTarget.className.replace(
+        "active",
+        ""
+      );
+    } else {
+      evt.currentTarget.className += " active";
+      document.getElementById(content_id).style.display = "block";
+    }
+  }
+}
